95d18d9f51667a8a505edb1b9a2733c64e26d050
Hideki Abe <hideki.abe.0630@gmail.com>
2013-08-25 22:15:59 +0900

    Testing model initialization

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,9 +1,23 @@
 "use strict";
 
-describe('Todo', function () {
-    describe('Application', function () {
-        it('名前空間オブジェクトが作成されること', function () {
+describe("Todo", function () {
+    describe("Application", function () {
+        it("名前空間オブジェクトが作成されること", function () {
             expect(app).to.be.ok();
+        });
+    });
+    
+    describe("Initialization", function () {
+        beforeEach(function () {
+            this.todo = new app.Todo();
+        });
+
+        it("デフォルトステータスが'pending'であること", function () {
+            expect(this.todo.get("completed")).to.be(false);
+        });
+
+        it("デフォルトのタイトルが空であること", function () {
+            expect(this.todo.get("title")).to.be.empty();
         });
     });
 });
