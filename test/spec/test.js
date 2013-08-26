18ef421c990fc499314152463e228589eb025d4b
Hideki Abe <abe@alpharook.jp>
2013-08-26 17:19:11 +0900

    Testing todo list item view

--- test/spec/test.js
+++ test/spec/test.js
@@ -20,4 +20,48 @@ describe("Todo", function () {
             expect(this.todo.get("title")).to.be.empty();
         });
     });
+
+    describe("Todo List Item View", function () {
+        beforeEach(function () {
+            this.todo = new app.Todo({ title: "test" });
+            this.item = new app.TodoView({ model: this.todo });
+            this.save_stub = sinon.stub(this.todo, "save");
+        });
+
+        afterEach(function () {
+            this.save_stub.restore();
+        });
+
+        it("render()がviewオブジェクトを返すこと", function () {
+            expect(this.item.render()).to.equal(this.item);
+        });
+
+        it("リストアイテムがレンダリングされること", function () {
+            expect(this.item.render().el.nodeName).to.equal("LI");
+        });
+
+        describe("Template", function () {
+            beforeEach(function () {
+                this.item.render();
+            });
+
+            it("タイトルがテキスト形式で存在すること", function () {
+                expect(this.item.$el.find("label").text()).to.equal('test');
+            });
+
+            it("完了のチェックボックスが含まれること", function () {
+                expect(this.item.$el.find(".toggle")).to.have.length(1);
+            });
+
+            it("完了のチェックボックスがチェックされていないこと", function () {
+                expect(this.item.$el.find(".toggle").is(":checked")).to.be(false);
+            });
+
+            it("todoを完了にできること", function () {
+                this.todo.set("completed", true);
+                this.item.render();
+                expect(this.item.$el.find(".toggle").is(":checked")).to.be(true);
+            });
+        });
+    });
 });
